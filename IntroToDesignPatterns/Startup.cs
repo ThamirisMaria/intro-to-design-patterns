b122039a0d31f987dbea08de7b75f3da8ea2c853
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2022-07-12 22:44:39 -0300

    Update Startup.cs: Adds swagger startup configs

--- IntroToDesignPatterns/Startup.cs
+++ IntroToDesignPatterns/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,6 +28,19 @@ namespace IntroToDesignPatterns
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "IntroToDesignPatterns",
+                    Description = "API - Frotas",
+                    Version = "v1"
+                });
+
+                var apiPath = Path.Combine(AppContext.BaseDirectory, "IntroToDesignPatterns.xml");
+                c.IncludeXmlComments(apiPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +50,12 @@ namespace IntroToDesignPatterns
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IntroToDesignPatterns");
+            });
 
             app.UseHttpsRedirection();
 
