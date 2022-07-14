4605431425cf20423eea71d5b28fc17cedcb4bdc
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2022-07-13 23:15:10 -0300

    Add singleton class

---
+++ IntroToDesignPatterns.Infra/Singleton/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroToDesignPatterns.Infra.Singleton
+{
+    public sealed class Singleton
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+    }
+}
